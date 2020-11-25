58908882e972835d47aa9766cd3818504e4c5825
Max Kuhn <mxkuhn@gmail.com>
2020-11-25 18:40:07 -0500

    verbose option

--- R/tune-functions.R
+++ R/tune-functions.R
@@ -9,6 +9,9 @@
 #' @param seed A single integer that is set before each workflow is processed.
 #' @param ... Common arguments that will be passed to each `tune` function. These
 #'  should not be the same arguments passed to `options`.
+#' @param verbose A single logical to indicate if logging should occur. This
+#'  will write a message _after_ each workflow is process and indicates if there
+#'  were any issues.
 #' @return An object from the corresponding `tune` package function (e.g. with
 #'  class `tune_result`, etc.)
 #' @examples
@@ -73,24 +76,25 @@
 #' }
 #' @export
 tune_grid.workflow_set <- function(object, pick = NULL, options = NULL,
-                                   seed = sample(1e5, 1), ...) {
-   fn_loop(object, .fn = "tune_grid", tune = TRUE, pick = pick,
+                                   seed = sample(1e5, 1), verbose = FALSE,
+                                   ...) {
+   fn_loop(object, .fn = "tune_grid", tune = TRUE, pick = pick, verbose = verbose,
            options = options, seed = seed, ...)
 }
 
 #' @export
 #' @rdname tune_grid.workflow_set
 tune_bayes.workflow_set <- function(object, pick = NULL, options = NULL,
-                                    seed = sample(1e5, 1), ...) {
-   fn_loop(object, .fn = "tune_bayes", tune = TRUE, pick = pick,
+                                    seed = sample(1e5, 1), verbose = FALSE, ...) {
+   fn_loop(object, .fn = "tune_bayes", tune = TRUE, pick = pick, verbose = verbose,
            options = options, seed = seed, ...)
 }
 
 #' @export
 #' @rdname tune_grid.workflow_set
 fit_resamples.workflow_set <- function(object, pick = NULL, options = NULL,
-                                       seed = sample(1e5, 1), ...) {
-   fn_loop(object, .fn = "fit_resamples", tune = FALSE, pick = pick,
+                                       seed = sample(1e5, 1), verbose = FALSE, ...) {
+   fn_loop(object, .fn = "fit_resamples", tune = FALSE, pick = pick, verbose = verbose,
            options = options, seed = seed, ...)
 }
 
@@ -98,7 +102,7 @@ fit_resamples.workflow_set <- function(object, pick = NULL, options = NULL,
 
 fn_loop <- function(object, .fn = "tune_grid", tune = TRUE,
                     pick = NULL, options = NULL, seed = sample(1e5, 1),
-                    ...) {
+                    verbose = FALSE, ...) {
    if (is.null(pick)) {
       has_tune <- purrr::map_lgl(object$objects, ~ nrow(tune::tune_args(.x)) > 0)
       no_value <- purrr::map_lgl(object$results, ~ length(.x) == 0)
@@ -119,15 +123,46 @@ fn_loop <- function(object, .fn = "tune_grid", tune = TRUE,
    dots <- rlang::enquos(...)
    options <- purrr::map(1:num_tasks, ~ c(options[[.x]], dots))
 
-   # TODO add a verbose option or progress bar
+   iter_seq <- seq_along(pick)
+   iter_chr <- format(iter_seq)
+   n <- length(iter_seq)
+
    for (iter in seq_along(pick)) {
       obj <- which(object$wflow_id == pick[iter])
       cl <- rlang::call2(.fn,
                          .ns = "tune",
                          object = object$objects[[obj]],!!!options[[iter]])
       withr::with_seed(seed[1],
-                       object$results[[obj]] <- rlang::eval_tidy(cl))
-
+                       object$results[[obj]] <-
+                          try(rlang::eval_tidy(cl), silent = TRUE))
+      log_progress(verbose, object$wflow_id[[obj]], object$results[[obj]],
+                   iter_chr[iter], n, .fn)
    }
    object
 }
+
+log_progress <- function(verbose, id, res, iter, n, .fn) {
+   if (!verbose) {
+      return(invisible(NULL))
+   }
+   cols <- tune::get_tune_colors()
+   event <- ifelse(grepl("tune", .fn), "tuning:    ", "resampling:")
+   msg <- paste0(iter, " of ", n, " ", event, " ", id)
+   if (inherits(res, "try-error")) {
+      message(
+         cols$symbol$danger(cli::symbol$cross), " ",
+         cols$message$info(msg),
+         cols$message$danger(" failed with "),
+         cols$message$danger(as.character(res))
+      )
+   } else {
+      message(
+         cols$symbol$success(cli::symbol$tick), " ",
+         cols$message$info(msg)
+      )
+   }
+
+   invisible(NULL)
+}
+
+
