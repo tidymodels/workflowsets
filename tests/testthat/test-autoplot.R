5fa5d5c1f0deac55fd5d443e6d2f00c0dfb7173d
Max Kuhn <mxkuhn@gmail.com>
2021-02-05 11:59:04 -0500

    changes for #23

--- tests/testthat/test-autoplot.R
+++ tests/testthat/test-autoplot.R
@@ -40,7 +40,7 @@ test_that("autoplot with without error bars", {
 })
 
 test_that("autoplot for specific workflow result", {
-   p_3 <- autoplot(chi_features_res, which = "plus_pca_lm")
+   p_3 <- autoplot(chi_features_res, id = "plus_pca_lm")
    p_4 <- autoplot(pull_workflow_result(chi_features_res, id = "plus_pca_lm"))
    expect_equal(p_3$data, p_4$data)
    expect_equal(p_3$labels, p_4$labels)
