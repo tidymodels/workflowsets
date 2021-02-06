34e37e304ac9510323f7b3f42a92b0bcc8865ba7
Max Kuhn <mxkuhn@gmail.com>
2021-02-05 19:18:10 -0500

    note -> comment for #26

--- tests/testthat/test-notes.R
+++ tests/testthat/test-notes.R
@@ -1,75 +1,75 @@
 
 # ------------------------------------------------------------------------------
 
-test_that("test notes", {
-   notes_1 <-
+test_that("test comments", {
+   comments_1 <-
       two_class_set %>%
-      note_add("none_cart", "What does 'cart' stand for\u2753")
+      comment_add("none_cart", "What does 'cart' stand for\u2753")
 
    expect_equal(
-      note_get(notes_1, id = "none_cart"),
+      comment_get(comments_1, id = "none_cart"),
       "What does 'cart' stand for\u2753"
    )
 
-   for(i in 2:nrow(notes_1)) {
-      expect_equal(notes_1$info[[i]]$note, character(1))
+   for(i in 2:nrow(comments_1)) {
+      expect_equal(comments_1$info[[i]]$comment, character(1))
    }
-   notes_2 <-
-      notes_1 %>%
-      note_add("none_cart", "Stuff.")
+   comments_2 <-
+      comments_1 %>%
+      comment_add("none_cart", "Stuff.")
    expect_equal(
-      note_get(notes_2, id = "none_cart") %>% paste0(collapse = "\n"),
+      comment_get(comments_2, id = "none_cart") %>% paste0(collapse = "\n"),
       "What does 'cart' stand for\u2753\nStuff."
    )
-   notes_3 <-
-      notes_2 %>%
-      note_reset("none_cart")
+   comments_3 <-
+      comments_2 %>%
+      comment_reset("none_cart")
    expect_equal(
-      notes_3$info[[1]]$note,
+      comments_3$info[[1]]$comment,
       character(1)
    )
    expect_equal(
-      two_class_set %>% note_add(),
+      two_class_set %>% comment_add(),
       two_class_set
    )
    expect_equal(
-      two_class_set %>% note_add("none_cart"),
+      two_class_set %>% comment_add("none_cart"),
       two_class_set
    )
    expect_error(
-      two_class_set %>% note_add("toe", "foot"),
+      two_class_set %>% comment_add("toe", "foot"),
       "The 'id' value is not in wflow_id."
    )
    expect_error(
-      two_class_set %>% note_add(letters, "foot"),
+      two_class_set %>% comment_add(letters, "foot"),
       "'id' should be a single character value."
    )
    expect_error(
-      two_class_set %>% note_add(1:2, "foot"),
+      two_class_set %>% comment_add(1:2, "foot"),
       "'id' should be a single character value."
    )
    expect_error(
-      two_class_set %>% note_add("none_cart", 1:2),
-      "The notes should be character strings."
+      two_class_set %>% comment_add("none_cart", 1:2),
+      "The comments should be character strings."
    )
    expect_error(
-      notes_1 %>% note_add("none_cart", "Stuff.", append = FALSE),
-      "There is already a note for this id and"
+      comments_1 %>% comment_add("none_cart", "Stuff.", append = FALSE),
+      "There is already a comment for this id and"
    )
    expect_error(
-      note_get(notes_1, id = letters),
+      comment_get(comments_1, id = letters),
       "should be a single character value"
    )
    expect_error(
-      note_get(notes_1, id = letters[1]),
+      comment_get(comments_1, id = letters[1]),
       "value is not in wflow_id"
    )
    expect_error(
-      notes_1 %>% note_reset(letters),
+      comments_1 %>% comment_reset(letters),
       "should be a single character value"
    )
    expect_error(
-      notes_1 %>% note_reset("none_carts"),
+      comments_1 %>% comment_reset("none_carts"),
       "value is not in wflow_id"
    )
 
