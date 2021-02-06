252158751212b4e8224ad0b9eafe531a13d3b23b
Max Kuhn <mxkuhn@gmail.com>
2021-02-05 19:29:06 -0500

    file name change and added snapshots

--- tests/testthat/test-comments.R
+++ tests/testthat/test-comments.R
@@ -74,3 +74,20 @@ test_that("test comments", {
    )
 
 })
+
+test_that("print comments", {
+
+   gatsby_1 <- "\"Whenever you feel like criticizing any one,\" he told me, \"just remember that all the people in this world haven't had the advantages that you've had.\""
+   gatsby_2 <- "My family have been prominent, well-to-do people in this middle-western city for three generations. The Carraways are something of a clan and we have a tradition that we're descended from the Dukes of Buccleuch, but the actual founder of my line was my grandfather's brother who came here in fifty-one, sent a substitute to the Civil War and started the wholesale hardware business that my father carries on today."
+   gatsby_3 <- "Across the courtesy bay the white palaces of fashionable East Egg glittered along the water, and the history of the summer really begins on the evening I drove over there to have dinner with the Tom Buchanans. Daisy was my second cousin once removed and I'd known Tom in college. And just after the war I spent two days with them in Chicago."
+
+   test <-
+      two_class_res %>%
+      comment_add("none_cart", gatsby_1) %>%
+      comment_add("none_cart", gatsby_2) %>%
+      comment_add("none_glm", gatsby_3)
+
+   expect_snapshot(comment_print(test))
+   expect_snapshot(comment_print(test, id = "none_glm"))
+})
+
