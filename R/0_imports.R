bb4d6d25f68ea83a064599bfd5d250dfc295b854
Max Kuhn <mxkuhn@gmail.com>
2021-06-27 12:43:18 -0400

    remove unneeded re-exports

--- R/0_imports.R
+++ R/0_imports.R
@@ -25,18 +25,6 @@ tibble::type_sum
 #' @export
 tibble::size_sum
 
-#' @importFrom tune tune_grid
-#' @export
-tune::tune_grid
-
-#' @importFrom tune tune_bayes
-#' @export
-tune::tune_bayes
-
-#' @importFrom tune fit_resamples
-#' @export
-tune::fit_resamples
-
 #' @importFrom tune collect_metrics
 #' @export
 tune::collect_metrics
